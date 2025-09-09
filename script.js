0e9e9036dfa2d458ee89a4d9098594171c1a1b24
GUILHERME TARIFA IOLE <guilherme.iole@gmail.com>
2025-09-09 17:58:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 function openTab(evt, tabName) {
-  // Esconde todas as abas
+
   var tabcontent = document.getElementsByClassName("tab-content");
   for (var i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove a classe "active" de todos os botões
+
   var tabbuttons = document.getElementsByClassName("tab-button");
   for (var i = 0; i < tabbuttons.length; i++) {
     tabbuttons[i].classList.remove("active");
   }
 
-  // Mostra a aba clicada
+  
   document.getElementById(tabName).style.display = "block";
 
-  // Marca o botão como ativo
   evt.currentTarget.classList.add("active");
 }
 
-// Quando a página carrega, exibe a primeira aba
 document.addEventListener("DOMContentLoaded", function() {
   var firstTabButton = document.querySelector(".tab-button");
   if (firstTabButton) {
@@ -28,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-// Adiciona o evento de clique a todos os botões de aba
+
 var tabButtonsContainer = document.getElementById("tab-buttons");
 if (tabButtonsContainer) {
   tabButtonsContainer.addEventListener("click", function(event) {
@@ -36,4 +34,5 @@ if (tabButtonsContainer) {
       openTab(event, event.target.getAttribute("data-tab"));
     }
   });
+
 }
